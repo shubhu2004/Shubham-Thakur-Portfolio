1016e68db8df4173b89b979da1957602b493c492
shubhu2004 <93043395+shubhu2004@users.noreply.github.com>
2024-09-01 21:44:39 +0530

    changed 404 to fix reload file not found error

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Shubham-Thakur-Portfolio/', // This is the key addition
+  base: '/Shubham-Thakur-Portfolio/', 
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
 })
