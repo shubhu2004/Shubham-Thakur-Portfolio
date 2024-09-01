2911c7fea92e42e76e02a226aac3efdbf625c117
shubhu2004 <93043395+shubhu2004@users.noreply.github.com>
2024-09-01 23:12:32 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//    base: '/Shubham-Thakur-Portfolio/', 
+//   plugins: [react()],
+//   assetsInclude: ['**/*.glb'],
+
+// })
+
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Shubham-Thakur-Portfolio/', 
+  base: process.env.NODE_ENV === 'production' ? '/Shubham-Thakur-Portfolio/' : '/',
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-})
+});
